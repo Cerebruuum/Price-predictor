ff0b4d336c478910721f3ec3d98825a508407a85
Omphile Tladi <rorisangtladi63@gmail.com>
2025-07-20 17:44:41 +0200

    Training pipeline and handling missing values

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -1,1 +1,14 @@
-from zenml import Model, 
+from steps.data_ingestion_step import data_ingestion_step
+from steps.handling_missing_values_step import handle_missing_values_step
+from zenml import Model, pipeline, step
+
+@pipeline(
+    model=Model(
+        name="prices_predictor"
+    ),
+)
+
+def ml_pipline():
+    raw_data = data_ingestion_step(file_path = "/Users/omphiletladi/Price-predictor/Data/archive2.zip")
+
+    filled_data = handle_missing_values_step(raw_data)
