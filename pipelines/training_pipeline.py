77239f01a92ba8d7ac1404f69e85be29ab0555fa
Omphile Tladi <rorisangtladi63@gmail.com>
2025-07-21 21:28:00 +0200

    Feature engineering and training pipeline

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -1,5 +1,6 @@
 from steps.data_ingestion_step import data_ingestion_step
 from steps.handling_missing_values_step import handle_missing_values_step
+from steps.feature_engineering_step import feature_engineering_step
 from zenml import Model, pipeline, step
 
 @pipeline(
@@ -12,3 +13,5 @@ def ml_pipline():
     raw_data = data_ingestion_step(file_path = "/Users/omphiletladi/Price-predictor/Data/archive2.zip")
 
     filled_data = handle_missing_values_step(raw_data)
+
+    engineered_data = feature_engineering_step(filled_data, strategy="log", features=["Gr Liv Area", "SalePrice"])
